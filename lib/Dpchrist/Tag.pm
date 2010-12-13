4f41c9f4f1a9bc62a0c4e3dfff3d80df85038419
David Paul Christensen <dpchrist@holgerdanske.com>
2010-12-13 02:56:46 +0000

    Import of DPCHRIST/Dpchrist-Tag-1.038 from CPAN.
    gitpan-cpan-distribution: Dpchrist-Tag
    gitpan-cpan-version:      1.038
    gitpan-cpan-path:         DPCHRIST/Dpchrist-Tag-1.038.tar.gz
    gitpan-cpan-author:       DPCHRIST
    gitpan-cpan-maturity:     released

--- lib/Dpchrist/Tag.pm
+++ lib/Dpchrist/Tag.pm
@@ -1,5 +1,5 @@
 #######################################################################
-# $Id: Tag.pm,v 1.37 2010-12-03 05:13:51 dpchrist Exp $
+# $Id: Tag.pm,v 1.38 2010-12-13 02:55:22 dpchrist Exp $
 #######################################################################
 # package/ Export:
 #----------------------------------------------------------------------
@@ -28,7 +28,7 @@ our @EXPORT_OK = (
 
 our @EXPORT = qw();
 
-our $VERSION = sprintf "%d.%03d", q$Revision: 1.37 $ =~ /(\d+)/g;
+our $VERSION = sprintf "%d.%03d", q$Revision: 1.38 $ =~ /(\d+)/g;
 
 #######################################################################
 # uses:
@@ -47,7 +47,7 @@ Dpchrist::Tag - labels for debug, log, etc., messages
 
 =head1 DESCRIPTION
 
-This documentation describes module revision $Revision: 1.37 $.
+This documentation describes module revision $Revision: 1.38 $.
 
 
 This is alpha test level software
